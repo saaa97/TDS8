dd18edcf760dadeebcd3bed70691ab7f53d9e43b
saaa97 <143739545+saaa97@users.noreply.github.com>
2023-08-31 23:12:49 +0530

    Update_tds_python.py

--- tds_python.py
+++ tds_python.py
@@ -1,16 +1,19 @@
-pip install streamlit
 import streamlit as st
 
-def find_largest_number(num1, num2, num3):
-    return max(num1, num2, num3)
+def find_largest(a, b, c):
+    return max(a, b, c)
 
-st.title("Find the Largest Number")
+def main():
+    st.title("Find the Largest Number")
 
-num1 = st.number_input("Enter the first number:")
-num2 = st.number_input("Enter the second number:")
-num3 = st.number_input("Enter the third number:")
+    st.write("Enter three numbers:")
+    a = st.number_input("Enter the first number:")
+    b = st.number_input("Enter the second number:")
+    c = st.number_input("Enter the third number:")
 
-if st.button("Find Largest Number"):
-    largest = find_largest_number(num1, num2, num3)
-    st.write(f"The largest number is: {largest}")
-streamlit run largest_number_app.py
+    if st.button("Find Largest"):
+        largest = find_largest(a, b, c)
+        st.write(f"The largest number is: {largest}")
+
+if __name__ == "__main__":
+    main()
